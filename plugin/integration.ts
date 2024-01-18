e1d76bf408c46b1a0b46160c0dc5349727d6fac6
Luiz Ferraz <luiz@lferraz.com>
2024-01-17 22:03:24 -0300

    Error pages

--- plugin/integration.ts
+++ plugin/integration.ts
@@ -1,12 +1,13 @@
 import type {AstroIntegration, RouteData} from "astro";
 import {fileURLToPath} from "node:url";
 
+
 type Options = {
     ghostRoutes: string[],
 }
 
 export default function ghostRouteIntegration(options: Options): AstroIntegration {
-    const missingRoutes = new Set(options.ghostRoutes);
+    const ghostRoutes = new Set(options.ghostRoutes);
 
     const ghostEntrypoint = fileURLToPath(new URL('./ghost-route.astro', import.meta.url));
 
@@ -31,18 +32,63 @@ export default function ghostRouteIntegration(options: Options): AstroIntegratio
                             },
                             load(id) {
                                 if (id === 'virtual:ghost-route-registry') {
-                                    return `export const routeRegistry = ${JSON.stringify(Array.from(missingRoutes))};`;
+                                    return `export const routeRegistry = ${JSON.stringify(Array.from(ghostRoutes))};`;
                                 }
-                            }
+                            },
+                            configureServer: command === 'dev' ? makeDevMiddleware(ghostRoutes) : undefined,
                         }]
                     },
                 })
             },
             'astro:build:setup': ({pages, updateConfig}) => {
                 for (const page of pages.values()) {
-                    missingRoutes.delete(page.route.route);
+                    ghostRoutes.delete(page.route.route);
                 }
             },
+        }
+    };
+}
+
+function makeDevMiddleware(ghostRoutes: Set<string>) {
+    return {
+        order: 'pre',
+        handler: (server) => {
+            server.middlewares.use((req, res, next) => {
+                function handleImplicitHeaders(method: Function) {
+                    return function (...args) {
+                        if (!res.headersSent) {
+                            const code = res.statusCode;
+                            // Don't handle error 500 because Vite's dev server has a
+                            // special handle for it anyway.
+                            if (code >= 400 && code !== 500 && ghostRoutes.has(`/${code}`)) {
+                                res.statusCode = 302;
+                                res.setHeader('location', `/${code}`);
+
+                                return method.apply(res, args);
+                            }
+                        }
+                        return method.apply(res, args);
+                    }
+                }
+
+                res.send = handleImplicitHeaders(res.send);
+                res.write = handleImplicitHeaders(res.write);
+                res.end = handleImplicitHeaders(res.end);
+
+                const writeHead = res.writeHead;
+
+                res.writeHead = function (code, ...args) {
+                    // Don't handle error 500 because Vite's dev server has a
+                    // special handle for it anyway.
+                    if (code >= 400 && code !== 500 && ghostRoutes.has(`/${code}`)) {
+                        res.setHeader('location', `/${code}`);
+                        code = 302;
+                    }
+                    return writeHead.call(res, code, ...args);
+                };
+
+                next();
+            });
         }
     };
 }
